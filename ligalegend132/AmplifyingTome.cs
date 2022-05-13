2323c870a764e439d6a8a1326193084203fec59e
3110476 <3110476@TE-GDA-D-D37H03>
2022-05-13 14:11:37 +0200

    Dodaj pliki projektów.

---
+++ ligalegend132/AmplifyingTome.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LolClasses
+{
+   
+    public class AmplifyingTome : Item
+    {
+        
+        public AmplifyingTome()
+        {
+           
+            name = "Amplifying Tome";
+            
+            price = 435;
+            
+            AddStat(Stat.AP, 20);
+        }
+    }
+}
---
+++ ligalegend132/Program.cs
@@ -0,0 +1,29 @@
+﻿using LolClasses;
+
+//stwórz nowego champa - garena
+Garen g = new Garen();
+//opdal emotke dla garena
+g.ctrl3();
+
+//stworz instacje przedmiotu - rubycrystal
+RubyCrystal rb = new RubyCrystal();
+AmplifyingTome at = new AmplifyingTome();
+//sprawdz ile ma hp garen
+g.showHP();
+g.showAP();
+//kup mu item
+g.buyItem(rb);
+//sprawdz ile ma teraz hp
+g.showHP();
+//kup mu drugi item
+g.buyItem(rb);
+g.buyItem(at);
+//sprawdz ile ma teraz hp
+g.showHP();
+g.showAP();
+
+
+
+
+
+
